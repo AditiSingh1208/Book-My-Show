1f09eb38793123c04e4d545125d4efe681643789
00a20.j50 <00a20.j50@gmail.com>
2024-08-14 15:37:46 +0000

    fix the error

--- src/components/MovieHero/MovieHero.Component.jsx
+++ src/components/MovieHero/MovieHero.Component.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { MovieContext } from "../components/context/Movie.context";
+import { MovieContext } from '../context/Movie.context';
 
 const MovieHero = () => {
   const { movie } = useContext(MovieContext);
